5f5df0ed75eb053671c7df4c04b9298305c97355
vicky1234500 <vicz.yang@gmail.com>
2025-04-12 12:27:09 +0800

    Add Friends component and integrate with Chats page

--- src/pages/Chats.jsx
+++ src/pages/Chats.jsx
@@ -1,19 +1,42 @@
 // import StaffScheduleTable from '../components/StaffScheduleTable';
 import Srcbar from "../components/Srcbar";
 import Filter from "../components/Filter";
+import Friends from "../components/Friends.jsx";
+import Profile from "../assets/user7.jpg";
 
 export default function Chats() {
+    const friendsData = [
+        {image: Profile, name: "Rachel Green", message: "Hell yea, dude!"},
+        {image: Profile, name: "Rachel Green", message: "Hell yea, dude!"},
+        {image: Profile, name: "Rachel Green", message: "Hell yea, dude!"},
+        {image: Profile, name: "Rachel Green", message: "Hell yea, dude!"},
+        {image: Profile, name: "Rachel Green", message: "Hell yea, dude!"},
+        {image: Profile, name: "Rachel Green", message: "Hell yea, dude!"},
+        {image: Profile, name: "Rachel Green", message: "Hell yea, dude!"},
+    ];
+
     return (
         <>
         <div className="pb-4">
             <h1 className="">Message</h1>
         </div>
             <Srcbar/>
-            <div className="flex gap-2 my-2">
+            <div className="flex gap-2 py-4">
                 <Filter color="bg-green-300" text="All" />
-                <Filter color="bg-green-300" text="Groups" />
-                <Filter color="bg-green-300" text="Unread" />
-                <Filter color="bg-green-300" text="Favorites" />
+                <Filter color="bg-gray-300" text="Groups" />
+                <Filter color="bg-gray-300" text="Unread" />
+                <Filter color="bg-gray-300" text="Favorites" />
+            </div>
+            <div>
+                {friendsData.map((friend, index) => (
+                    <Friends
+                        key={index}
+                        image={friend.image}
+                        name={friend.name}
+                        message={friend.message}
+                        onClick={() => console.log(`Clicked on ${friend.name}`)}
+                    />
+                ))}
             </div>
         </>
     );
